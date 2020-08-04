ee74827cf186d51b1a5d1c35505d697dbc509908
Abdul Rehman <mehars.6925@gmail.com>
2020-08-05 03:01:29 +0500

    top navigation for the application

---
+++ resources/js/components/partials/Navbar.vue
@@ -0,0 +1,37 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container">
+            <router-link class="navbar-brand" :to="{name: 'home'}">
+                <img :src="logo" height="60" alt="Orama">
+            </router-link>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item">
+                    <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link class="nav-link" :to="{name: 'signup'}">Sign up</router-link>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+</template>
+
+<script>
+    export default {
+        name: 'Navbar',
+        data() {
+            return {
+                logo: require('@/images/ezbunk.0caa7f64.png')
+            }
+        }
+    }
+</script>
