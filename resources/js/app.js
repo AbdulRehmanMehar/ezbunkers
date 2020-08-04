284ed4043ced9ae70cff518898610833c9f9cdde
Abdul Rehman <mehars.6925@gmail.com>
2020-08-05 03:00:53 +0500

    added jqurey, bootstrap, popper and font-awesome

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,6 +1,35 @@
+window.$ = window.jQuery = require('jquery')
+require('popper.js')
+require('bootstrap')
+
 import Vue from 'vue'
 import App from '@/App.vue'
 import router from '@/router'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import {
+    faThumbsUp,
+    faForward,
+    faCheckCircle,
+    faSearch,
+    faGlobeAmericas,
+    faSms,
+    faPhone
+} from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+
+library.add(
+    faThumbsUp,
+    faForward,
+    faCheckCircle,
+    faSearch,
+    faGlobeAmericas,
+    faSms,
+    faPhone
+)
+
+Vue.component('font-awesome-icon', FontAwesomeIcon)
+
+Vue.config.productionTip = false
 
 new Vue({
     router,
