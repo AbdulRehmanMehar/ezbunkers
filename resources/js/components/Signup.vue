1cde74cb06ae6aa984a7b100c5439dc2d40c2e40
Abdul Rehman <mehars.6925@gmail.com>
2020-08-08 18:06:39 +0500

    Signup View

--- resources/js/components/Signup.vue
+++ resources/js/components/Signup.vue
@@ -1,11 +1,30 @@
 <template>
     <div>
-        <h1>Signup!</h1>
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="card">
+                        <div class="card-header">Sign Up</div>
+                        <div class="card-body">
+                            <ContactInformation />
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
+
     export default {
-        name: 'Signup'
+        name: 'Signup',
+
+        components: {
+            Success: () => import('@/components/partials/signup/Success.vue'),
+            PersonalInformation: () => import('@/components/partials/signup/PersonalInformation.vue'),
+            ContactInformation: () => import('@/components/partials/signup/ContactInformation.vue'),
+            CompanyInformation: () => import('@/components/partials/signup/CompanyInformation.vue'),
+        }
     }
 </script>
