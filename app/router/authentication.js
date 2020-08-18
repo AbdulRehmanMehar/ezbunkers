6415a195e9eacdb0e7d72a4b88f326e32b693b43
Abdul Rehman <mehars.6925@gmail.com>
2020-08-18 17:44:10 +0500

    updated for otp verification

--- app/router/authentication.js
+++ app/router/authentication.js
@@ -3,14 +3,19 @@ const fs = require('fs')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const { v4: uuidv4 } = require('uuid')
+const OTPModel = require('../models/otp')
 const router = require('express').Router()
 const FileModel = require('../models/file')
+const generateOTP = require('../config/otp')
+const AdminModel = require('../models/admin')
 const AccountModel = require('../models/account')
 const { check, validationResult } = require('express-validator')
+const authenticatedMiddleware = require('../middlewares/authenticated')
+
 
 router.post('/login',
 [
-    check('uid').notEmpty().withMessage('Please enter user id.')
+    check('uid').notEmpty().withMessage('Please enter user id (received via email).')
         .custom(async (value, {req}) => {
             let account = await AccountModel.findOne({ uid: req.body.uid })
             if (account && account._id.toString()) {
@@ -65,7 +70,41 @@ router.post('/login',
 })
 
 
-router.post('/register', [
+router.get('/see-exists-and-set-otp/:uid',
+async (req, res) => {
+    try {
+        let status = 204
+        let account = await AccountModel.findOne({ uid: req.params.uid })
+
+        if (account) {
+            status = 200
+
+            if (!account.password) {
+                let theOTP = await OTPModel({
+                    code: generateOTP(),
+                    account: account._id
+                }).save()
+
+                await AccountModel.findByIdAndUpdate(account._id, {otp: theOTP._id})
+
+                status = 201
+            }
+
+
+        }
+
+        return res.status(status)
+
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+})
+
+
+router.post('/register',
+[
     check('name').trim().escape().notEmpty().withMessage('Name is required'),
     check('email').isEmail().withMessage('Enter a valid email')
         .custom(async (value) => {
@@ -75,6 +114,15 @@ router.post('/register', [
             }
             return true
         }),
+
+    check('role').trim().escape().custom(value => {
+        let lower = value.toString().toLowerCase()
+        if (lower === 'shipping' || lower == 'bunkering') {
+            return true
+        }
+
+        throw new Error('Invalid Role is Selected')
+    }),
     check('phone').isMobilePhone().withMessage('Enter a valid phone number'),
     check('imo').isMobilePhone().withMessage('Enter a valid imo number'),
     check('companyName').trim().escape().notEmpty().withMessage('Enter company name'),
@@ -93,7 +141,8 @@ router.post('/register', [
         throw new Error('Select documents for proof of Ownership')
     }),
 
-], async (req, res) => {
+],
+async (req, res) => {
 
     const errors = validationResult(req)
     if (!errors.isEmpty()) {
@@ -135,6 +184,7 @@ router.post('/register', [
         }
 
         let account = await AccountModel({
+            type: req.body.role,
             name: req.body.name,
             email: req.body.email,
             phone: req.body.phone,
@@ -153,6 +203,84 @@ router.post('/register', [
             error
         })
     }
+})
+
+
+router.patch('/set-password',
+authenticatedMiddleware,
+[
+    check('password').notEmpty().withMessage('Password is required').custom((value, {req}) => {
+        if (value == req.body.confirmPassword) {
+            return true
+        }
+        throw new Error('Passwords don\'t match')
+    })
+],
+async (req, res) => {
+
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            errors: errors.array()
+        })
+    }
+
+    try {
+        await OTPModel.findOneAndDelete({ account: req.account._id })
+        await AccountModel.findByIdAndUpdate(req.account._id, {
+            password: bcrypt.hashSync(req.body.password),
+            otp: null
+        })
+
+        return res.status(200)
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+})
+
+
+router.post('/admin-login',
+[
+    check('username').notEmpty().withMessage('Please provide username')
+        .custom(async (value, {req}) => {
+            let admin = await AdminModel.findOne({ username: value })
+            if (admin) {
+                req.admin = admin
+                return true
+            }
+            throw new Error('Username is not in record')
+        }),
+    check('password').notEmpty().withMessage('Please provide password')
+        .custom(async (value, {req}) => {
+            if (bcrypt.compareSync(value, req.admin.password)) {
+                return true
+            }
+            throw new Error('Password is not valid')
+        })
+],
+(req, res) => {
+    const errors = validationResult(req)
+    if (!errors.isEmpty()) {
+        return res.status(422).json({
+            errors: errors.array()
+        })
+    }
+
+    let admin = req.admin
+
+    delete admin.password
+
+    const token = jwt.sign(admin, process.env.APP_SECRET)
+    admin.token = token
+
+    return res.status(200).json({
+        data: {
+            admin
+        }
+    })
+
 })
 
 
