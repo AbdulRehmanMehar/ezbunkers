75811ae99ffed1c4a8e5a0b28868d304c0910596
Abdul Rehman <mehars.6925@gmail.com>
2020-08-18 17:48:31 +0500

    imported routers

--- app/router/index.js
+++ app/router/index.js
@@ -1,7 +1,13 @@
 const router = require('express').Router()
+const AdminRouter = require('./admin')
+const VesselRouter = require('./vessel')
+const NominationRouter = require('./nomination')
 const AuthenticationRouter = require('./authentication')
 
 
+router.use('/admin', AdminRouter)
+router.use('/vessel', VesselRouter)
+router.use('/nomination', NominationRouter)
 router.use('/authentication', AuthenticationRouter)
 
 
