2c38af539931fdcb9d2bfc57d192693163115281
Abdul Rehman <mehars.6925@gmail.com>
2020-08-26 13:54:13 +0500

    basic layout for accounts

---
+++ resources/js/components/admin/dashcomponents/Accounts.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+
+    <vue-good-table :columns="columns" :rows="accountRows" :pagination-options="{
+      enabled: true,
+      mode: 'records',
+      perPage: 5,
+      position: 'top',
+      perPageDropdown: [3, 7, 9],
+      dropdownAllowAll: false,
+      setCurrentPage: 1,
+      nextLabel: 'next',
+      prevLabel: 'prev',
+      rowsPerPageLabel: 'Rows per page',
+      ofLabel: 'of',
+      pageLabel: 'page', // for 'pages' mode
+      allLabel: 'All',
+    }" />
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardAccounts",
+
+  data() {
+    return {
+      columns: [
+
+      ]
+    }
+  },
+
+
+  computed: {
+    accountRows() {
+      return []
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
