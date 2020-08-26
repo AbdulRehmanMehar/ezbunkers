294199a42a5f63c5cb95a451986ecb8091ebdda9
Abdul Rehman <mehars.6925@gmail.com>
2020-08-26 13:54:40 +0500

    dashboard root child view

---
+++ resources/js/components/admin/dashcomponents/DashboardIndex.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+
+    <div class="card my-5">
+      <div class="card-body py-4">
+        <h1>Accounts</h1>
+        <p>Activate or Deactivate accounts</p>
+        <router-link style="float: right" :to="{ name: 'admin-dashboard-accounts' }" class="btn btn-info">Go</router-link>
+      </div>
+    </div>
+
+    <div class="card my-5">
+      <div class="card-body py-4">
+        <h1>Fuel</h1>
+        <p>Add fuel types</p>
+        <router-link style="float: right" :to="{ name: 'admin-dashboard-fuel' }" class="btn btn-info">Go</router-link>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "DashboardIndex"
+}
+</script>
+
+<style scoped>
+
+</style>
