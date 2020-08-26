725082df148323d5266bf5f7103ed9fd0e8d5c6e
Abdul Rehman <mehars.6925@gmail.com>
2020-08-26 13:54:32 +0500

    dashboard view

---
+++ resources/js/components/admin/Dashboard.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+
+    <div class="container my-5">
+      <div class="row">
+        <div class="col-md-8 offset-md-2">
+
+          <router-view></router-view>
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AdminDashboard"
+}
+</script>
+
+<style scoped>
+
+</style>
