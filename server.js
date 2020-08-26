2a8894d2bd84ebe880789adbc896a238b6edd0d6
Abdul Rehman <mehars.6925@gmail.com>
2020-08-26 13:48:03 +0500

    removed express fileupload

--- server.js
+++ server.js
@@ -1,24 +1,23 @@
 require('dotenv').config()
+const path = require('path')
+const cors = require('cors')
 const http = require('http')
 const morgan = require('morgan')
 const express = require('express')
 const mongoose = require('mongoose')
 const ApiRouter = require('./app/router')
 const bodyParser = require('body-parser')
-const fileUpload = require('express-fileupload')
 
 const app = express()
 const server = http.createServer(app)
 
 mongoose.connect(process.env.DATABASE_URI, { useNewUrlParser: true, useUnifiedTopology: true })
 
+app.use(cors())
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
-app.use(fileUpload({
-    useTempFiles : true,
-    tempFileDir : `${__dirname}/app/uploads/temp`
-}))
+
 app.use(morgan(process.env.DEBUG ? 'dev' : 'combined'))
 app.use(express.static('public'))
 
