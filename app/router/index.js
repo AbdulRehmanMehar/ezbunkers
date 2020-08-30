68a683677ec2e4084209236ad51de9a6ad30f5c1
Abdul Rehman <mehars.6925@gmail.com>
2020-08-31 02:35:36 +0500

    handles some routes

--- app/router/index.js
+++ app/router/index.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const AdminRouter = require('./admin')
 const VesselRouter = require('./vessel')
+const FuelModel = require('../models/fuel')
 const NominationRouter = require('./nomination')
 const AuthenticationRouter = require('./authentication')
 
@@ -11,4 +12,38 @@ router.use('/nomination', NominationRouter)
 router.use('/authentication', AuthenticationRouter)
 
 
+router.get('/fuels',
+async (req, res) => {
+    try {
+        let fuels = await FuelModel.find()
+
+        return res.status(200).json({
+            data: {
+                fuels
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+})
+
+
+router.get('/accounts',
+async (req, res) => {
+    try {
+        let accounts = await AccountModel.find()
+
+        return res.status(200).json({
+            data: {
+                accounts
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+})
 module.exports = router
