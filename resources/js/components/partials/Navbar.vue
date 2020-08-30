473231f25a6338c2619d554fdbd8e4995c4e67f7
Abdul Rehman <mehars.6925@gmail.com>
2020-08-31 02:38:48 +0500

    updated

--- resources/js/components/partials/Navbar.vue
+++ resources/js/components/partials/Navbar.vue
@@ -2,42 +2,41 @@
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <div class="container">
             <router-link class="navbar-brand" :to="{name: 'home'}">
-                <img :src="logo" height="60" alt="Orama">
+                <img src="/images/ezbunk.0caa7f64.png" height="60" alt="Orama">
             </router-link>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <ul class="navbar-nav ml-auto">
+            <ul class="navbar-nav ml-auto" v-if="adminAccount">
                 <li class="nav-item">
                     <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
                 </li>
 
-                <div  style="display: inherit" v-if="adminAccount">
+                <li class="nav-item">
+                    <router-link class="nav-link" :to="{name: 'admin-dashboard'}">Admin Dashboard</router-link>
+                </li>
 
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{name: 'admin-dashboard'}">Admin Dashboard</router-link>
-                    </li>
-
-                    <li class="nav-item">
-                        <a href="#" class="nav-link" @click.prevent="logoutAdmin">Logout</a>
-                    </li>
-
-                </div>
-
-                <div style="display: inherit" v-else>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{name: 'how-it-works'}">How it Works</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{name: 'signup'}">Sign Up</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link class="nav-link" :to="{name: 'login'}">Log In</router-link>
-                    </li>
-                </div>
+                <li class="nav-item">
+                    <a href="#" class="nav-link" @click.prevent="logoutAdmin">Logout</a>
+                </li>
             </ul>
+
+              <ul class="navbar-nav ml-auto" v-else>
+                <li class="nav-item">
+                  <router-link class="nav-link" :to="{name: 'home'}">Home</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" :to="{name: 'how-it-works'}">How it Works</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" :to="{name: 'signup'}">Sign Up</router-link>
+                </li>
+                <li class="nav-item">
+                  <router-link class="nav-link" :to="{name: 'login'}">Log In</router-link>
+                </li>
+              </ul>
         </div>
         </div>
     </nav>
@@ -46,11 +45,6 @@
 <script>
     export default {
         name: 'Navbar',
-        data() {
-            return {
-                logo: require('@/images/ezbunk.0caa7f64.png')
-            }
-        },
 
         computed: {
           adminAccount() {
