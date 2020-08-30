ef7263027d9d4f3c52a08c161fe50193491296e0
Abdul Rehman <mehars.6925@gmail.com>
2020-08-31 03:53:44 +0500

    now handles login

--- resources/js/components/Login.vue
+++ resources/js/components/Login.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <div class="container my-5">
+    <div v-if="!loading" class="container my-5">
       <div class="row">
         <div class="col-md-8 offset-md-2">
           <div class="card">
@@ -8,28 +8,31 @@
               Login
             </div>
             <div class="card-body">
-              <form @submit.prevent="">
+              <form @submit.prevent="handleLogin">
 
 
                 <div class="form-group">
                   <label for="inputName">User ID</label>
-                  <input type="text" v-model="uid" class="form-control" id="inputName" placeholder="HkNkS7PoLW32bjBR" required>
+                  <input type="text" v-model="uid" class="form-control" id="inputName" placeholder="HkNkS7PoLW32bjBR" required :readonly="(seeUserIdAndSetOTPIfNeededResponseCode != 204 && seeUserIdAndSetOTPIfNeededResponseCode != 0)">
                   <small v-if="errors.length > 0 && errors.find(error => error.param == 'uid')" class="form-text text-danger">
                     {{ errors.find(error => error.param == 'uid').msg }}
                   </small>
+                  <small v-if="seeUserIdAndSetOTPIfNeededResponseCode == 204" class="text-danger">
+                    No such User was found!
+                  </small>
                 </div>
 
-                <div class="form-group" v-if="!userIdValidated || accountExistsResponseCode == 204">
-                  <button type="submit" class="btn form-control btn-outline-info">Validate</button>
+                <div class="form-group" v-if="seeUserIdAndSetOTPIfNeededResponseCode == 0 || seeUserIdAndSetOTPIfNeededResponseCode == 204">
+                  <button type="submit" class="btn form-control btn-outline-info">Next</button>
                 </div>
 
-                <div v-else>
+                <div v-else-if="seeUserIdAndSetOTPIfNeededResponseCode == 201 || seeUserIdAndSetOTPIfNeededResponseCode == 200">
 
-                  <div class="form-group" v-if="accountExistsResponseCode == 201">
+                  <div class="form-group" v-if="seeUserIdAndSetOTPIfNeededResponseCode == 201">
                     <label for="inputOTP">One Time Password</label>
                     <input type="text" v-model="otp" class="form-control" id="inputOTP" placeholder="*******" required>
-                    <small v-if="errors.length > 0 && errors.find(error => error.param == 'otp')" class="form-text text-danger">
-                      {{ errors.find(error => error.param == 'otp').msg }}
+                    <small v-if="errors.length > 0 && errors.find(error => error.param == 'password')" class="form-text text-danger">
+                      {{ errors.find(error => error.param == 'password').msg }}
                     </small>
                   </div>
 
@@ -46,19 +49,30 @@
                   </div>
                 </div>
 
+
+                <div class="text-danger" v-else>
+                  Sorry, Something went seriously wrong.
+                </div>
               </form>
             </div>
           </div>
         </div>
       </div>
     </div>
+
+    <div v-else>
+      <Loading />
+      <h1 class="mt-4 text-center">Processing</h1>
+    </div>
   </div>
 </template>
 
 <script>
+import Loading from "@/components/partials/Loading"
+
 export default {
   name: "Login",
-
+  components: { Loading },
   data() {
     return {
       uid: null,
@@ -69,16 +83,49 @@ export default {
 
   computed: {
     errors() {
-      return []
+      return this.$store.getters['Login/errors'] || []
     },
-    userIdValidated() {
-      return false
+
+    loading() {
+      return this.$store.getters['Login/loading']
     },
-    accountExistsResponseCode() {
-      return 201
+
+    seeUserIdAndSetOTPIfNeededResponseCode() {
+      return this.$store.getters['Login/validate_uid_response_code']
     },
-    nextURI() {
-      return 'set-password'
+
+  },
+
+  methods: {
+    handleLogin() {
+      if (this.otp == null && this.password == null && this.uid != null) { // We need to Validate
+        this.seeUserIdAndSetOTPIfNeeded()
+      } else {
+
+
+        if (this.seeUserIdAndSetOTPIfNeededResponseCode == 201) {
+          this.$store.dispatch('Login/do_login_request', {
+            uid: this.uid,
+            otp: this.otp
+          }).then(() => {
+            this.$router.push({ name: 'set-account-password' })
+          })
+        } else {
+          this.$store.dispatch('Login/do_login_request', {
+            uid: this.uid,
+            password: this.password
+          }).then(() => {
+            this.$router.push({ name: 'home' })
+          })
+        }
+
+
+
+      }
+    },
+
+    seeUserIdAndSetOTPIfNeeded() {
+      this.$store.dispatch('Login/validate_user_id', this.uid)
     }
   }
 }
