16346d0e892e5a9da4ce850f85639e85dabf78d2
Abdul Rehman <mehars.6925@gmail.com>
2020-08-31 19:42:13 +0500

    updated

--- app/config/mails/static/stylesheet.js
+++ app/config/mails/static/stylesheet.js
@@ -1,268 +1,233 @@
 module.exports = () => {
     return `
-        html {
-\twidth:100%
-}
-::-moz-selection {
-background:#fd4326;
-color:#fff;
-text-shadow:1px 1px 0 #f22b0e
-}
-::selection {
-background:#fd4326;
-color:#fff;
-text-shadow:1px 1px 0 #f22b0e
-}
-body {
-\tbackground-color:#fff;
-\tmargin:0;
-\tpadding:0
-}
-.ReadMsgBody {
-\twidth:100%;
-\tbackground-color:#fff
-}
-.ExternalClass {
-\twidth:100%;
-\tbackground-color:#fff
-}
-a {
-\tcolor:#fd4326;
-\ttext-decoration:none;
-\tfont-weight:400;
-\tfont-style:normal
-}
-a:hover {
-\tcolor:#262626;
-\ttext-decoration:none;
-\tfont-weight:400;
-\tfont-style:normal
-}
-p, div {
-\tmargin:0!important
-}
-table {
-\tborder-collapse:collapse
-}
-@media only screen and (max-width:640px) {
-body {
-width:auto!important
-}
-table table {
-width:100%!important
-}
-td[class=full_width] {
-width:100%!important
-}
-td[class=spacer] {
-width:30px!important
-}
-td[class=spacer_spec] {
-display:none!important
-}
-div[class=div_scale] {
-width:440px!important;
-margin:0 auto!important
-}
-table[class=table_scale] {
-width:440px!important;
-margin:0 auto!important
-}
-td[class=td_scale] {
-width:440px!important;
-margin:0 auto!important
-}
-img[class=img_scale] {
-width:100%!important;
-height:auto!important
-}
-img[class=divider] {
-width:100%!important;
-height:2px!important
-}
-td[class=divider] {
-width:100%!important;
-display:block!important;
-float:left;
-text-align:inherit!important
-}
-}
-@media only screen and (max-width:479px) {
-body {
-width:auto!important
-}
-table table {
-width:100%!important
-}
-td[class=full_width] {
-width:100%!important
-}
-div[class=div_scale] {
-width:280px!important;
-margin:0 auto!important
-}
-table[class=table_scale] {
-width:280px!important;
-margin:0 auto!important
-}
-td[class=td_scale] {
-width:280px!important;
-margin:0 auto!important
-}
-img[class=img_scale] {
-width:100%!important;
-height:auto!important
-}
-img[class=divider] {
-width:100%!important;
-height:2px!important
-}
-td[class=spacer] {
-display:none!important
-}
-td[class=spacer_spec] {
-display:none!important
-}
-td[class=divider] {
-width:100%!important;
-display:block!important;
-float:left;
-text-align:inherit!important
-}
-td[class=center] {
-text-align:center!important
-}
-td[class=subject_line] {
-float:left;
-width:240px;
-display:block!important;
-text-align:left!important;
-padding:15px 20px!important
-}
-td[class=contact] {
-float:left;
-width:240px;
-display:block!important;
-text-align:left!important;
-padding:0 20px 15px!important;
-padding-bottom:20px!important
-}
-td[class=social_left] {
-float:left;
-width:240px;
-display:block!important;
-text-align:center!important;
-padding:20px 20px 0!important
-}
-td[class=social_right] {
-float:left;
-width:240px;
-display:block!important;
-text-align:center!important;
-padding:0 20px!important
-}
-td[class=one_one] {
-width:240px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-padding-right:20px!important;
-text-align:inherit!important
-}
-td[class=one_half] {
-width:240px!important;
-padding-bottom:0!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_half_last] {
-width:240px!important;
-margin-top:40px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important;
-padding-top:0!important
-}
-td[class=one_third_fed] {
-width:240px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_third_fed_sec] {
-width:240px!important;
-margin-top:20px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_third_fed_last] {
-width:240px!important;
-margin-top:20px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_third] {
-width:240px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_third_sec] {
-width:240px!important;
-margin-top:40px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=two_third_last] {
-width:240px!important;
-margin-top:40px!important;
-display:block!important;
-float:left;
-padding-left:20px!important;
-text-align:inherit!important
-}
-td[class=two_third] {
-width:240px!important;
-display:block!important;
-float:left;
-margin-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_third_last] {
-width:240px!important;
-margin-top:40px!important;
-display:block!important;
-float:left;
-margin-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_fourth] {
-width:110px!important;
-display:block!important;
-float:left;
-margin-left:20px!important;
-text-align:inherit!important
-}
-td[class=one_fourth_last] {
-width:110px!important;
-margin-top:20px!important;
-display:block!important;
-float:left;
-margin-left:20px!important;
-text-align:inherit!important
-}
-}
-    
-    `
+    /* -------------------------------------
+          GLOBAL RESETS
+      ------------------------------------- */
+      img {
+        border: none;
+        -ms-interpolation-mode: bicubic;
+        max-width: 100%; }
+      body {
+        background-color: #f6f6f6;
+        font-family: sans-serif;
+        -webkit-font-smoothing: antialiased;
+        font-size: 14px;
+        line-height: 1.4;
+        margin: 0;
+        padding: 0; 
+        -ms-text-size-adjust: 100%;
+        -webkit-text-size-adjust: 100%; }
+      table {
+        border-collapse: separate;
+        mso-table-lspace: 0pt;
+        mso-table-rspace: 0pt;
+        width: 100%; }
+        table td {
+          font-family: sans-serif;
+          font-size: 14px;
+          vertical-align: top; }
+      /* -------------------------------------
+          BODY & CONTAINER
+      ------------------------------------- */
+      .body {
+        background-color: #f6f6f6;
+        width: 100%; }
+      /* Set a max-width, and make it display as block so it will automatically stretch to that width, but will also shrink down on a phone or something */
+      .container {
+        display: block;
+        Margin: 0 auto !important;
+        /* makes it centered */
+        max-width: 580px;
+        padding: 10px;
+        width: 580px; }
+      /* This should also be a block element, so that it will fill 100% of the .container */
+      .content {
+        box-sizing: border-box;
+        display: block;
+        Margin: 0 auto;
+        max-width: 580px;
+        padding: 10px; }
+      /* -------------------------------------
+          HEADER, FOOTER, MAIN
+      ------------------------------------- */
+      .main {
+        background: #fff;
+        border-radius: 3px;
+        width: 100%; }
+      .wrapper {
+        box-sizing: border-box;
+        padding: 20px; }
+      .footer {
+        clear: both;
+        padding-top: 10px;
+        text-align: center;
+        width: 100%; }
+        .footer td,
+        .footer p,
+        .footer span,
+        .footer a {
+          color: #999999;
+          font-size: 12px;
+          text-align: center; }
+      /* -------------------------------------
+          TYPOGRAPHY
+      ------------------------------------- */
+      h1,
+      h2,
+      h3,
+      h4 {
+        color: #000000;
+        font-family: sans-serif;
+        font-weight: 400;
+        line-height: 1.4;
+        margin: 0;
+        Margin-bottom: 30px; }
+      h1 {
+        font-size: 35px;
+        font-weight: 300;
+        text-align: center;
+        text-transform: capitalize; }
+      p,
+      ul,
+      ol {
+        font-family: sans-serif;
+        font-size: 14px;
+        font-weight: normal;
+        margin: 0;
+        Margin-bottom: 15px; }
+        p li,
+        ul li,
+        ol li {
+          list-style-position: inside;
+          margin-left: 5px; }
+      a {
+        color: #3498db;
+        text-decoration: underline; }
+      /* -------------------------------------
+          BUTTONS
+      ------------------------------------- */
+      .btn {
+        box-sizing: border-box;
+        width: 100%; }
+        .btn > tbody > tr > td {
+          padding-bottom: 15px; }
+        .btn table {
+          width: auto; }
+        .btn table td {
+          background-color: #ffffff;
+          border-radius: 5px;
+          text-align: center; }
+        .btn a {
+          background-color: #ffffff;
+          border: solid 1px #3498db;
+          border-radius: 5px;
+          box-sizing: border-box;
+          color: #3498db;
+          cursor: pointer;
+          display: inline-block;
+          font-size: 14px;
+          font-weight: bold;
+          margin: 0;
+          padding: 12px 25px;
+          text-decoration: none;
+          text-transform: capitalize; }
+      .btn-primary table td {
+        background-color: #3498db; }
+      .btn-primary a {
+        background-color: #3498db;
+        border-color: #3498db;
+        color: #ffffff; }
+      /* -------------------------------------
+          OTHER STYLES THAT MIGHT BE USEFUL
+      ------------------------------------- */
+      .last {
+        margin-bottom: 0; }
+      .first {
+        margin-top: 0; }
+      .align-center {
+        text-align: center; }
+      .align-right {
+        text-align: right; }
+      .align-left {
+        text-align: left; }
+      .clear {
+        clear: both; }
+      .mt0 {
+        margin-top: 0; }
+      .mb0 {
+        margin-bottom: 0; }
+      .preheader {
+        color: transparent;
+        display: none;
+        height: 0;
+        max-height: 0;
+        max-width: 0;
+        opacity: 0;
+        overflow: hidden;
+        mso-hide: all;
+        visibility: hidden;
+        width: 0; }
+      .powered-by a {
+        text-decoration: none; }
+      hr {
+        border: 0;
+        border-bottom: 1px solid #f6f6f6;
+        Margin: 20px 0; }
+      /* -------------------------------------
+          RESPONSIVE AND MOBILE FRIENDLY STYLES
+      ------------------------------------- */
+      @media only screen and (max-width: 620px) {
+        table[class=body] h1 {
+          font-size: 28px !important;
+          margin-bottom: 10px !important; }
+        table[class=body] p,
+        table[class=body] ul,
+        table[class=body] ol,
+        table[class=body] td,
+        table[class=body] span,
+        table[class=body] a {
+          font-size: 16px !important; }
+        table[class=body] .wrapper,
+        table[class=body] .article {
+          padding: 10px !important; }
+        table[class=body] .content {
+          padding: 0 !important; }
+        table[class=body] .container {
+          padding: 0 !important;
+          width: 100% !important; }
+        table[class=body] .main {
+          border-left-width: 0 !important;
+          border-radius: 0 !important;
+          border-right-width: 0 !important; }
+        table[class=body] .btn table {
+          width: 100% !important; }
+        table[class=body] .btn a {
+          width: 100% !important; }
+        table[class=body] .img-responsive {
+          height: auto !important;
+          max-width: 100% !important;
+          width: auto !important; }}
+      @media all {
+        .ExternalClass {
+          width: 100%; }
+        .ExternalClass,
+        .ExternalClass p,
+        .ExternalClass span,
+        .ExternalClass font,
+        .ExternalClass td,
+        .ExternalClass div {
+          line-height: 100%; }
+        .apple-link a {
+          color: inherit !important;
+          font-family: inherit !important;
+          font-size: inherit !important;
+          font-weight: inherit !important;
+          line-height: inherit !important;
+          text-decoration: none !important; } 
+        .btn-primary table td:hover {
+          background-color: #34495e !important; }
+        .btn-primary a:hover {
+          background-color: #34495e !important;
+          border-color: #34495e !important; } }
+     `
 }
--- app/config/mails/templates/account-activation.js
+++ app/config/mails/templates/account-activation.js
@@ -1,103 +1,87 @@
 const stylesheet = require('../static/stylesheet')
 
 module.exports = (uid) => {
-    return `
-        <style>
-            ${stylesheet()}
-        </style>
 
-<table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#d0d0d0" style="padding: 0; margin: 0;">
-  <!-- START OF TOP BAR-->
-  <tr>
-    <td class="full_width" align="center" width="100%" bgcolor="#d0d0d0" style=""><div class="div_scale" style="width:600px;">
-        <table class="table_scale" width="600" HEIGHT="42" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#494949" style="padding:0; margin: 0;">
-          <tr>
-            <td class="spacer" width="20" align="left" valign="top" bgcolor="#494949" style="margin: 0 !important; padding: 0 !important; line-height: 0 !important;">&nbsp;</td>
-            <!-- START OF SUBJECT LINE-->
-            <td class="subject_line" align="left" valign="middle" width="270" bgcolor="#494949" style="padding-top: 10px; padding-bottom: 10px;"><table width="100%">
-                <tr>
-                  <td class="center" align="" valign="" style="font-family:Arial, sans-serif; font-style: italic; color:#d8d8d8; font-size:11px; line-height:18px;">EzBukners</td>
-                </tr>
-              </table></td>
-            <!-- END OF SUBJECT LINE-->
-            <td class="spacer" width="20" align="left" valign="top" bgcolor="#494949" style="margin: 0 !important; padding: 0 !important; line-height: 0 !important;">&nbsp;</td>
-            <!-- START OF CONTACT-->
-            <td class="contact" align="right" valign="middle" width="270" bgcolor="#494949" style="padding: 0px;"><table width="100%">
-                <tr>
-<!--                  <td class="center" align="" valign="" style="text-align: right; font-family:Arial, sans-serif; font-style: italic; color:#d8d8d8; font-size:11px; line-height:100%;"><img src="https://i.imgur.com/RxR11qU.png?1" alt="email" width="20" height="11" style="display: inline; vertical-align: middle;" />wallace.gleen@gmail.com </td>-->
-                </tr>
-              </table></td>
-            <!-- END OF CONTACT-->
-            <td class="spacer" width="20" align="left" valign="top" bgcolor="#494949" style="margin: 0 !important; padding: 0 !important; line-height: 0 !important;">&nbsp;</td>
-          </tr>
-        </table>
-      </div></td>
-  </tr>
-  <!-- END OF TOP BAR-->
-  <!-- START OF VERTIXAL SPACER BLOCK-->
-  <tr>
-    <td class="full_width" align="center" width="100%" bgcolor="#d0d0d0" style=""><div class="div_scale" style="width:600px;">
-        <table class="table_scale" width="600" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#d0d0d0" style="padding:0; margin: 0;">
-          <tr>
-            <td class="td_scale" width="600" height="40" bgcolor="#d0d0d0" align="center" valign="middle" style="height: 40px; padding: 0px; font-size:0 ; color:#686868; font-family: Arial,sans-serif; line-height: 0; ">&nbsp;</td>
-          </tr>
-        </table>
-      </div></td>
-  </tr>
-  <!-- END OF VERTIXAL SPACER BLOCK-->
-  <!-- START OF FEATURED AREA BLOCK-->
-  <tr>
-    <td class="full_width" align="center" width="100%" bgcolor="#d0d0d0" style=""><div class="div_scale" style="width:600px;">
-        <table class="table_scale" width="600" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#f0f0f0" style="padding:0; margin: 0;">
-          <tr>
-            <!-- START OF LEFT COLUMN-->
-            <td class="td_scale" width="600" bgcolor="#fd4326" align="center" valign="top" style="padding: 0px; font-size:14px ; color:#959595; font-family: Arial,sans-serif; line-height: 24px; "><table width="100%" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#fd4326" style="margin: 0;">
-                
-                <!-- START OF VERTICAL SPACER-->
-                <tr>
-                  <td height="20" bgcolor="#f0f0f0" style="padding:0; line-height: 0;">&nbsp;</td>
-                </tr>
-                <!-- END OF VERTICAL SPACER-->
-                <!-- START OF HEADING TITLE-->
-                <tr>
-                  <td class="center" align="center" valign="top" bgcolor="#f0f0f0" style="padding: 0px 20px;  text-shadow: 1px 1px 0px #ffffff;font-size:24px ; color:#444444; font-family: Lucida Sans Unicode; line-height: 34px; ">Your account is activated!</td>
-                </tr>
-                <!-- END OF HEADING TITLE-->
-                <!-- START OF VERTICAL SPACER-->
-                <tr>
-                  <td height="10" bgcolor="#f0f0f0" style="padding:0; line-height: 0;">&nbsp;</td>
-                </tr>
-                <!-- END OF VERTICAL SPACER-->
-                <!-- START OF TEXT-->
-                <tr>
-                  <td class="center" align="center" valign="top" bgcolor="#f0f0f0" style="padding: 0px 20px;  text-shadow: 1px 1px 0px #ffffff;font-size:13px ; color:#727272; font-family: Arial,sans-serif; line-height: 23px; ">This email is to inform you that your account is successfully activated. You can use ${uid} as your login username!</td>
-                </tr>
-                <!-- END OF TEXT-->
-                <!-- START OF VERTICAL SPACER-->
-                <tr>
-                  <td height="20" bgcolor="#f0f0f0" style="padding:0; line-height: 0;">&nbsp;</td>
-                </tr>
-                <!-- END OF VERTICAL SPACER-->
-              </table></td>         
-      <!-- JUST A VERTICAL SPACER-->
-      <div class="div_scale" style="width:600px; display: block; border-bottom: 1px solid #d0d0d0;">
-        <table class="table_scale" width="600" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#f9f9f9" style="padding:0; margin: 0;">
-          <tr>
-            <td class="td_scale" width="600" height="20" bgcolor="#f9f9f9" align="center" valign="middle" style="height: 20px; padding: 0px; font-size:0 ; color:#686868; font-family: Arial,sans-serif; line-height: 0; ">&nbsp;</td>
-          </tr>
-        </table>
-      </div>
-      <!-- END OF JUST A VERTICAL SPACER-->
-      <!-- JUST A VERTICAL SPACER-->
-      <div class="div_scale" style="width:600px; display: block; ">
-        <table class="table_scale" width="600" border="0" align="center" cellpadding="0" cellspacing="0" bgcolor="#f0f0f0" style="padding:0; margin: 0;">
-          <tr>
-            <td class="td_scale" width="600" height="40" bgcolor="#f0f0f0" align="center" valign="middle" style="height: 40px; padding: 0px; font-size:0 ; color:#686868; font-family: Arial,sans-serif; line-height: 0; ">&nbsp;</td>
-          </tr>
-        </table>
-      </div>
-      <!-- END OF JUST A VERTICAL SPACER--></td>
-  </tr>
-</table>
-    `;
+    return `
+        <!doctype html>
+        <html>
+          <head>
+            <meta name="viewport" content="width=device-width" />
+            <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+            <title>Simple Transactional Email</title>
+            <style>${stylesheet()}</style>
+          </head>
+          
+          <body class="">
+            <table border="0" cellpadding="0" cellspacing="0" class="body">
+              <tr>
+                <td>&nbsp;</td>
+                <td class="container">
+                  <div class="content">
+                    <span class="preheader">EzBunkers</span>
+                    <table class="main">
+        
+                      <!-- START MAIN CONTENT AREA -->
+                      <tr>
+                        <td class="wrapper">
+                          <table border="0" cellpadding="0" cellspacing="0">
+                            <tr>
+                              <td>
+                                <h1>Account Activation</h1>
+                                <h2>Hooray, Admin just approved your account!</h2>
+                                <table border="0" cellpadding="0" cellspacing="0">
+                                  <tbody>
+                                    <tr>
+                                      <td align="left">
+                                        <table border="0" cellpadding="0" cellspacing="0">
+                                          <tbody>
+                                            <tr>
+                                              <td> <p> Use <code>${uid}</code> as User ID to login to the website. Once you try to login using the User ID, you'll be sent an email with OTP. You'll use the OTP to login and set password.</p> </td>
+                                            </tr>
+                                          </tbody>
+                                        </table>
+                                      </td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                                <p>If you received this email by mistake, simply delete it.</p>
+              
+                              </td>
+                            </tr>
+                          </table>
+                        </td>
+                      </tr>
+        
+                    <!-- END MAIN CONTENT AREA -->
+                    </table>
+        
+                    <!-- START FOOTER -->
+                    <div class="footer">
+                      <table border="0" cellpadding="0" cellspacing="0">
+                        <tr>
+                          <td class="content-block">
+                            <span class="apple-link">EzBunkers</span>
+                            <br> &nbsp; <a href="#">&nbsp;</a>.
+                          </td>
+                        </tr>
+                        <tr>
+                          <td class="content-block powered-by">
+                            &nbsp;
+                          </td>
+                        </tr>
+                      </table>
+                    </div>
+                    <!-- END FOOTER -->
+                    
+                  <!-- END CENTERED WHITE CONTAINER -->
+                  </div>
+                </td>
+                <td>&nbsp;</td>
+              </tr>
+            </table>
+          </body>
+        </html>
+    
+    `
+
 }
