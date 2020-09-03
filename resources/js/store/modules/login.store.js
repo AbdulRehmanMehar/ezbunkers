3ffc906df8d5f20143f25bfef9d3220c6f43dc16
Abdul Rehman <mehars.6925@gmail.com>
2020-09-04 02:39:30 +0500

    store

--- resources/js/store/modules/login.store.js
+++ resources/js/store/modules/login.store.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import Vue from "vue";
 
 
 const state = {
@@ -28,6 +29,30 @@ const mutations = {
 
     SET_VALIDATE_UID_RESPONSE_CODE(state, payload) {
         state.validate_uid_response_code = payload
+    },
+
+    ADD_DOCUMENTS(state, payload) {
+        let docs = [...state.account.companyDocuments, ...payload]
+        state.account.companyDocuments = docs
+        Vue.set(state.account, 'companyDocuments', docs)
+    },
+
+    DELETE_DOCUMENT(state, payload) {
+        let document = state.account.companyDocuments.filter(document => document._id == payload)[0]
+        state.account.companyDocuments = state.account.companyDocuments.filter(document => document._id != payload)
+        Vue.delete(state.account.companyDocuments, document)
+    },
+
+    ADD_IMAGES(state, payload) {
+        let images = [...state.account.companyImages, ...payload]
+        state.account.companyImages = images
+        Vue.set(state.account, 'companyImages', images)
+    },
+
+    DELETE_IMAGE(state, payload) {
+        let image = state.account.companyImages.filter(image => image._id == payload)[0]
+        state.account.companyImages = state.account.companyImages.filter(image => image._id != payload)
+        Vue.delete(state.account.companyImages, image)
     },
 }
 
@@ -79,6 +104,110 @@ const actions = {
         })
     },
 
+    do_document_add_request({ commit, rootState, rootGetters }, data) {
+        return new Promise((resolve, reject) => {
+            commit('SET_ERRORS', null)
+            commit('SET_LOADING', true)
+            commit('SET_SUCCESS', false)
+            axios.post('/api/authentication/add-documents', data, {
+                headers: {
+                    authorization: rootGetters['Login/account'].token
+                }
+            }).then(resp => {
+                commit('ADD_DOCUMENTS', resp.data.data.documents)
+                commit('SET_SUCCESS', true)
+                commit('SET_LOADING', false)
+
+                localStorage.setItem('user-account', JSON.stringify(rootGetters['Login/account']))
+                resolve(resp)
+            }).catch(error => {
+                console.log(error)
+                commit('SET_ERRORS', error)
+                commit('SET_LOADING', false)
+                reject(error)
+            })
+        })
+    },
+
+    do_document_delete_request({ commit, rootState, rootGetters }, data) {
+        return new Promise((resolve, reject) => {
+            commit('SET_LOADING', true)
+            commit('SET_SUCCESS', false)
+            commit('SET_ERRORS', null)
+
+            axios.delete('/api/authentication/document/' + data, {
+                headers: {
+                    authorization: rootGetters['Login/account'].token
+                }
+            })
+                .then(resp => {
+                    commit('SET_LOADING', false)
+                    commit('SET_SUCCESS', true)
+                    commit('DELETE_DOCUMENT',  data )
+                    localStorage.setItem('user-account', JSON.stringify(rootGetters['Login/account']))
+                    resolve(resp)
+                })
+                .catch(error => {
+                    commit('SET_LOADING', false)
+                    commit('SET_ERRORS', error)
+                    reject(error)
+                })
+
+        })
+    },
+
+    do_image_add_request({ commit, rootState, rootGetters }, data) {
+        return new Promise((resolve, reject) => {
+            commit('SET_ERRORS', null)
+            commit('SET_LOADING', true)
+            commit('SET_SUCCESS', false)
+            axios.post('/api/authentication/add-images', data, {
+                headers: {
+                    authorization: rootGetters['Login/account'].token
+                }
+            }).then(resp => {
+                commit('ADD_IMAGES', resp.data.data.images)
+                commit('SET_SUCCESS', true)
+                commit('SET_LOADING', false)
+
+                localStorage.setItem('user-account', JSON.stringify(rootGetters['Login/account']))
+                resolve(resp)
+            }).catch(error => {
+                console.log(error)
+                commit('SET_ERRORS', error)
+                commit('SET_LOADING', false)
+                reject(error)
+            })
+        })
+    },
+
+    do_image_delete_request({ commit, rootState, rootGetters }, data) {
+        return new Promise((resolve, reject) => {
+            commit('SET_LOADING', true)
+            commit('SET_SUCCESS', false)
+            commit('SET_ERRORS', null)
+
+            axios.delete('/api/authentication/image/' + data, {
+                headers: {
+                    authorization: rootGetters['Login/account'].token
+                }
+            })
+                .then(resp => {
+                    commit('SET_LOADING', false)
+                    commit('SET_SUCCESS', true)
+                    commit('DELETE_IMAGE',  data )
+                    localStorage.setItem('user-account', JSON.stringify(rootGetters['Login/account']))
+                    resolve(resp)
+                })
+                .catch(error => {
+                    commit('SET_LOADING', false)
+                    commit('SET_ERRORS', error)
+                    reject(error)
+                })
+
+        })
+    },
+
     logout({ commit }) {
         localStorage.removeItem('user-account')
         commit('SET_ACCOUNT', null)
@@ -90,7 +219,11 @@ const getters = {
     loading: (state) => state.loading,
     success: (state) => state.success,
     account: (state) => state.account,
+    images: (state, getters) => getters.account.companyImages,
+    documents: (state, getters) => getters.account.companyDocuments,
+    companyLogo: (state, getters) => getters.images.filter(image => image.type == 'logo')[0],
     validate_uid_response_code: (state) => state.validate_uid_response_code,
+
 }
 
 export default {
