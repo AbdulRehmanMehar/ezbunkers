b7739d8d1eb3dfd5612e30742de8dccd7dfb78c6
Abdul Rehman <mehars.6925@gmail.com>
2020-09-10 02:52:31 +0500

    orders store

---
+++ resources/js/store/modules/orders.store.js
@@ -0,0 +1,68 @@
+import axios from 'axios'
+
+
+const state = {
+    loading: false,
+    errors: null,
+    success: false
+}
+
+const mutations = {
+    SET_LOADING(state, payload) {
+        state.loading = payload
+    },
+
+    SET_ERRORS(state, payload) {
+        state.errors = payload
+    },
+
+    SET_SUCCESS(state, payload) {
+        state.success = payload
+    },
+
+}
+
+const actions = {
+
+    nominate({ commit, rootState, rootGetters }, data) {
+        return new Promise((resolve, reject) => {
+            commit('SET_LOADING', true)
+            commit('SET_SUCCESS', false)
+            commit('SET_ERRORS', null)
+
+            axios.post('/api/nomination', data, {
+                headers: {
+                    authorization: rootGetters['Login/account'].token
+                }
+            })
+                .then(resp => {
+                    commit('SET_LOADING', false)
+                    commit('SET_SUCCESS', true)
+                    console.log(resp)
+                    resolve(resp)
+                })
+                .catch(error => {
+                    commit('SET_LOADING', false)
+                    commit('SET_ERRORS', error)
+                    reject(error)
+                })
+
+        })
+    },
+
+}
+
+const getters = {
+    errors: (state) => state.errors,
+    loading: (state) => state.loading,
+    success: (state) => state.success,
+    fuels: (state) => state.fuels,
+}
+
+export default {
+    namespaced: true,
+    state,
+    getters,
+    actions,
+    mutations
+}
