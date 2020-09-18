9b0b5dc03fbb759dd4c84bf94f0b62edb40da58f
Abdul Rehman <mehars.6925@gmail.com>
2020-09-19 04:43:42 +0500

    updated to load participants

--- app/config/io.js
+++ app/config/io.js
@@ -1,20 +1,27 @@
 const jwt = require('jsonwebtoken')
 const socketIO = require('socket.io')
 const ChatModel = require('../models/chat')
+const AccountModel = require('../models/account')
 
 
-module.export = (server) => {
+module.exports = (server) => {
     let users = []
     let messages = []
 
     const io = socketIO(server)
 
-    io.use((socket, next) => {
+    io.use(async (socket, next) => {
         if (socket.handshake.query && socket.handshake.query.token) {
             const token = socket.handshake.query.token
-            jwt.verify(token, process.env.APP_SECRET, (error, payload) => {
+            await jwt.verify(token, process.env.APP_SECRET,  async (error, payload) => {
                 if (error) return next(new Error("Socket.io Authentication Error."))
-                socket.request.user = payload
+                console.log(payload)
+                let user = await AccountModel.findOne({ uid: payload.uid })
+                if (user) {
+                    socket.request.user = user
+                } else {
+                    return next(new Error("Socket.io Authentication Error."))
+                }
             })
             return next()
         }
@@ -23,13 +30,13 @@ module.export = (server) => {
 
     io.on('connection', (socket) => {
 
-        users.push({ sock: socket.id, uid: socket.request.user.uid })
+        users.push({ sock: socket.id, _id: socket.request.user._id })
 
         messages = messages.filter((message) => {
-            let user = users.find(user => user.uid == message.receiver)
+            let user = users.find(user => user._id == message.receiver)
 
             if (user) {
-                io.to(user.sock).emit(message)
+                io.sockets.to(user.sock).emit(message)
                 return false
             }
 
@@ -37,7 +44,48 @@ module.export = (server) => {
         })
 
 
-        io.on('conversation', async (data) => {
+        socket.on('participants', async (data) => {
+            try {
+                let participants = []
+
+                console.log(socket.request.user._id)
+                let results = await ChatModel.find({
+                    $or: [
+                        {sender: socket.request.user._id},
+                        {receiver: socket.request.user._id}
+                    ]
+                })
+
+
+                for(let result of results) {
+
+
+                    if (result.sender && result.sender._id.toString() == socket.request.user._id.toString()) {
+                        if (result.receiver)
+                            participants.push(result.receiver)
+                    } else {
+                        if (result.sender)
+                            participants.push(result.sender)
+                    }
+
+                }
+                participants = participants.reduce((acc, current) => {
+                    const x = acc.find(item => item._id.toString() === current._id.toString());
+                    if (!x) {
+                        return acc.concat([current]);
+                    } else {
+                        return acc;
+                    }
+                }, []);
+
+                socket.emit('participants', participants)
+
+            } catch (error) {
+                socket.emit('error', error)
+            }
+        })
+
+        socket.on('conversation', async (data) => {
             if (data.receiver) {
                 try {
                     let conversation = await ChatModel.find({
@@ -58,11 +106,15 @@ module.export = (server) => {
 
         socket.on('message', async (data) => {
             if (data.message && data.receiver && socket.request.user._id != data.receiver) {
-
                 try {
 
-                    let user = users.find((user) => user.uid == data.receiver)
+                    console.log(users)
 
+                    let user = users.find((user) => user._id == data.receiver)
+
+
+
+                    console.log('user', user)
                     let message = await ChatModel({
                         message: data.message,
                         sender: socket.request.user._id,
@@ -70,10 +122,12 @@ module.export = (server) => {
                     }).save()
 
                     if (user) {
-                        io.to(user.sock).emit('message', message)
+                        io.sockets.to(user.sock).emit('message', message)
+                        console.log(user.sock)
                     } else {
                         messages.push(message)
                     }
+                    socket.emit('message', message)
 
                 } catch (error) {
                     socket.emit('error', error)
@@ -86,7 +140,7 @@ module.export = (server) => {
 
         socket.on('disconnect', () => {
             users = users.filter(
-                o => o.socket !== socket.id
+                user => user.sock !== socket.id
             )
         })
 
