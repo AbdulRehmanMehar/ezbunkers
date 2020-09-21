f780ccb23fc42d2e96892591691c50dd11ffd648
Abdul Rehman <mehars.6925@gmail.com>
2020-09-21 05:26:48 +0500

    added routes for review

--- app/router/index.js
+++ app/router/index.js
@@ -1,12 +1,16 @@
 const router = require('express').Router()
 const AdminRouter = require('./admin')
 const VesselRouter = require('./vessel')
+const mailer = require('../config/mails')
 const FuelModel = require('../models/fuel')
 const VesselModel = require('../models/vessel')
 const NominationRouter = require('./nomination')
 const AccountModel = require('../models/account')
+const ReviewModel = require('../models/review')
+const NominationModel = require('../models/nomination')
 const AuthenticationRouter = require('./authentication')
-
+const authenticatedMiddleware = require('../middlewares/authenticated')
+const ReviewToOrderVesselOwner = require('../config/mails/templates/you-got-a-review')
 
 router.use('/admin', AdminRouter)
 router.use('/vessel', VesselRouter)
@@ -67,5 +71,57 @@ router.get('/vessels',
         }
     })
 
+
+// REVIEWS
+
+router.post('/review',
+authenticatedMiddleware,
+async (req, res) => {
+    try {
+        let review = await ReviewModel({
+            quality: req.body.quality,
+            price: req.body.price,
+            communication: req.body.communication,
+            review: req.body.review,
+            reviewee: req.body.reviewee,
+            reviewer: req.account._id
+        }).save()
+
+        await NominationModel.findOneAndUpdate({ _id: req.body.nomination }, { review: review })
+
+        await AccountModel.findOneAndUpdate({ _id: review.reviewee._id }, { review: review })
+
+        await mailer(review.reviewee.email, 'You got a Review', ReviewToOrderVesselOwner(review.reviewee.name, review.reviewer.companyName, review._id))
+
+        return res.status(201).json({
+            data: {
+                review
+            }
+        })
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+})
+
+
+router.get('/review',
+async (req, res) => {
+    try {
+        let reviews = await ReviewModel.find()
+
+        return res.status(200).json({
+            data: {
+                reviews
+            }
+        })
+
+    } catch (error) {
+        return res.status(500).json({
+            error
+        })
+    }
+})
 
 module.exports = router
